349c3a1379fecdb65fc59f703ed152ed335fa483
abdulqodir18 <ukkosha45@gmail.com>
2021-05-10 18:39:30 +0500

    a

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,15 +2,7 @@ import React from 'react'
 import Partners from '../../components/Partners/Partners'
 
 function Home() {
-<<<<<<< HEAD
   return <>s</>
-=======
-    return (
-        <>
-            <Partners />
-        </>
-    )
->>>>>>> f4b7039f3a845f719f9953e510916907c4cba1b1
 }
 
 export default Home
