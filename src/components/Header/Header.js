a4631aab595e6b75237d42b3dd1ffe5bde009108
jakhongirs <j.sagdullaev31@gmail.com>
2021-05-11 11:31:39 +0500

    Not fully finished sitenav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,50 +8,39 @@ import PhoneCall from '../Lib/Svg/PhoneCall'
 import Hamburger from '../Lib/Svg/Hamburger'
 
 function Header() {
-    return (
-        <header className='site-header'>
-            <Container>
-                <div className='site-header__wrapper'>
-                    <NavLink className='logo' to='/'>
-                        <LogoImg className='logo__img' />
-                    </NavLink>
+	return (
+		<header className='site-header'>
+			<Container>
+				<div className='site-header__wrapper'>
+					<NavLink className='logo' to='/'>
+						<LogoImg className='logo__img' />
+					</NavLink>
 
-                    <div className='site-header__features'>
-                        <a
-                            className='site-header__tel-link'
-                            href='tel:+998901234567'>
-                            <PhoneCall className='site-header__phonecall' />
-                            +998 90 123 45 67
-                        </a>
+					<div className='site-header__features'>
+						<a className='site-header__tel-link' href='tel:+998901234567'>
+							<PhoneCall className='site-header__phonecall' />
+							+998 90 123 45 67
+						</a>
 
-                        <select
-                            className='site-header__select-lang'
-                            name='select-lang'
-                            id='select-lang'>
-                            <option value='uz' selected>
-                                Uz
-                            </option>
-                            <option value='ru'>Ru</option>
-                            <option value='en'>En</option>
-                        </select>
+						<select
+							className='site-header__select-lang'
+							name='select-lang'
+							id='select-lang'>
+							<option value='uz' selected>
+								Uz
+							</option>
+							<option value='ru'>Ru</option>
+							<option value='en'>En</option>
+						</select>
 
-                        <Link
-                            className='site-header__request-button'
-                            to='/survey'>
-                            ОСТАВИТЬ ЗАЯВКУ
-                        </Link>
-                    </div>
-
-                    <button className='site-header__hamburger'>
-                        МЕНЮ
-                        <Hamburger className='site-header__hamburger-icon' />
-                    </button>
-
-                    <nav className='sitenav'></nav>
-                </div>
-            </Container>
-        </header>
-    )
+						<Link className='site-header__request-button' to='/survey'>
+							ОСТАВИТЬ ЗАЯВКУ
+						</Link>
+					</div>
+				</div>
+			</Container>
+		</header>
+	)
 }
 
 export default Header
