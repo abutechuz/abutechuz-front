bcbc436a9b563fb2ee01e23449ecdc153b32cbba
jakhongirs <j.sagdullaev31@gmail.com>
2021-05-12 12:20:54 +0500

    Statistics Section FInished

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Partners from '../../components/Partners/Partners'
 import Hero from '../../components/Hero/Hero'
+import Statistics from '../../components/Statistics/Statistics'
 
 function Home() {
 	return (
 		<>
 			<Hero />
-			{/* <Partners /> */}
+			<Statistics />
+			<Partners />
 		</>
 	)
 }
