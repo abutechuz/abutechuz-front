1cec2850f15f02f2478e1b26c59d689c7d7994eb
jakhongirs <j.sagdullaev31@gmail.com>
2021-05-15 18:38:56 +0500

    Site header fixed added responsive and hamburger

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ import './App.scss'
 import Articles from './screens/ARTICLES/Articles.js'
 
 function App() {
-    return (
-        <>
-            {/* <Header /> */}
-            <main>
-                <Switch>
-                    <Route path='/' component={Home} exact/>
-                    <Route path='/form' component={Form} exact />
-                    <Route path='/articles' component={Articles} exact />
-                    <Route path='/single-blog' component={SingleBlog} exact />
-                </Switch>
-            </main>
-            <Footer />
-        </>
-    )
+	return (
+		<>
+			<Header />
+			<main>
+				<Switch>
+					<Route path='/' component={Home} exact />
+					<Route path='/form' component={Form} exact />
+					<Route path='/articles' component={Articles} exact />
+					<Route path='/single-blog' component={SingleBlog} exact />
+				</Switch>
+			</main>
+			<Footer />
+		</>
+	)
 }
 
 export default App
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,10 +8,12 @@ import PhoneCall from '../Lib/Svg/PhoneCall'
 import Hamburger from '../Lib/Svg/Hamburger'
 
 function Header() {
+	const elHumburger = React.useRef()
+
 	return (
 		<header className='site-header'>
 			<Container>
-				<div className='site-header__wrapper'>
+				<div className='site-header__wrapper' ref={elHumburger}>
 					<NavLink className='logo' to='/'>
 						<LogoImg className='logo__img' />
 					</NavLink>
@@ -37,6 +39,45 @@ function Header() {
 							ОСТАВИТЬ ЗАЯВКУ
 						</Link>
 					</div>
+
+					<div className='site-header__background-effect'></div>
+
+					<button
+						className='site-header__hamburger'
+						onClick={() =>
+							elHumburger.current.classList.toggle(
+								'site-header__menu-list-open'
+							)
+						}>
+						МЕНЮ
+						<Hamburger className='hamburger__icon' />
+					</button>
+
+					<ul className='site-header__menu-list'>
+						<li className='site-header__menu-item'>
+							<Link className='site-header__menu-link'>Главная</Link>
+						</li>
+
+						<li className='site-header__menu-item'>
+							<Link className='site-header__menu-link'>О нас</Link>
+						</li>
+
+						<li className='site-header__menu-item'>
+							<Link className='site-header__menu-link'>Сервисы</Link>
+						</li>
+
+						<li className='site-header__menu-item'>
+							<Link className='site-header__menu-link'>Ресурсы</Link>
+						</li>
+
+						<li className='site-header__menu-item'>
+							<Link className='site-header__menu-link'>Проекты</Link>
+						</li>
+
+						<li className='site-header__menu-item'>
+							<Link className='site-header__menu-link'>Контакты</Link>
+						</li>
+					</ul>
 				</div>
 			</Container>
 		</header>
