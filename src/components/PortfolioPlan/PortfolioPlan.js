01e2f1da444fe9dab73ee58c65664bdac97b0f18
inspirationjon <ilhomshamidinov55@gmail.com>
2021-05-17 15:17:20 +0500

    Add plans page

---
+++ src/components/PortfolioPlan/PortfolioPlan.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import Container from '../Container/Container'
+import './PortfolioPlan.scss'
+
+function PortfolioPlan() {
+    return (
+        <section className='portfolio-plan'>
+            <Container>
+                <h2 className='portfoli-plan__heading'>Planning</h2>
+
+                <ul className='portfolio-plan__list'>
+                    <li className='portfolio-plan__item'>
+                        <h3 className='portfolio-plan__heading'>Input</h3>
+
+                        <h4 className='portfolio-plan__sub-heading'>
+                            The client initially had:
+                        </h4>
+
+                        <ul className='portfolio-plan__sublist'>
+                            <li className='portfolio-plan__sublist-item'>
+                                Ready-made Android app
+                            </li>
+                            <li className='portfolio-plan__sublist-item'>
+                                Android TV app that didn’t meet customer
+                                expectations
+                            </li>
+                            <li className='portfolio-plan__sublist-item'>
+                                Custom UI design.
+                            </li>
+                        </ul>
+                    </li>
+                    <li className='portfolio-plan__item'>
+                        <h3 className='portfolio-plan__heading'>Input</h3>
+
+                        <h4 className='portfolio-plan__sub-heading'>
+                            The client initially had:
+                        </h4>
+
+                        <ul className='portfolio-plan__sublist'>
+                            <li className='portfolio-plan__sublist-item'>
+                                Ready-made Android app
+                            </li>
+                            <li className='portfolio-plan__sublist-item'>
+                                Android TV app that didn’t meet customer
+                                expectations
+                            </li>
+                            <li className='portfolio-plan__sublist-item'>
+                                Custom UI design.
+                            </li>
+                        </ul>
+                    </li>
+                    <li className='portfolio-plan__item'>
+                        <h3 className='portfolio-plan__heading'>Input</h3>
+
+                        <h4 className='portfolio-plan__sub-heading'>
+                            The client initially had:
+                        </h4>
+
+                        <ul className='portfolio-plan__sublist'>
+                            <li className='portfolio-plan__sublist-item'>
+                                Ready-made Android app
+                            </li>
+                            <li className='portfolio-plan__sublist-item'>
+                                Android TV app that didn’t meet customer
+                                expectations
+                            </li>
+                            <li className='portfolio-plan__sublist-item'>
+                                Custom UI design.
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </Container>
+        </section>
+    )
+}
+
+export default PortfolioPlan
--- src/screens/SinglePortfolio/SinglePortfolio.js
+++ src/screens/SinglePortfolio/SinglePortfolio.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './SinglePortfolio.scss'
 import PortfolioDesc from '../../components/PortfolioDesc/PortfolioDesc'
 import PortfolioIntro from '../../components/PortfolioIntro/PortfolioIntro'
+import PortfolioPlan from '../../components/PortfolioPlan/PortfolioPlan'
 
 function SinglePortfolio() {
     return (
         <>
             <PortfolioIntro />
             <PortfolioDesc />
+            <PortfolioPlan />
         </>
     )
 }
